ff6f12d98836c510b3952e43b09b8c329f7df76a
Tarun Verma <151928619+Tarun-Va@users.noreply.github.com>
2025-07-31 18:01:07 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-import { BrowserRouter, HashRouter } from "react-router";
+import React from "react";
+import { HashRouter } from "react-router";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
   <HashRouter>
     <App />
   </HashRouter>
-  </BrowserRouter>
 )
